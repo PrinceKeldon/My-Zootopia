498a452179369de2255302615e039972649b754d
PrinceKeldon <dakiri_keldon@protonmail.com>
2026-03-16 11:28:38 +0100

    Serialize animals as list items html files

---
+++ My-Zootopia/animal_web_generator.py
@@ -0,0 +1,24 @@
+import json
+
+def load_data(file_path):
+    """Loads a JSON file"""
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+
+animals_data = load_data("animals_data.json")
+
+output = ""
+for animal in animals_data:
+    output += '<li Class="cards_item">\n'
+
+    if "name" in animal:
+        output += f"Name: {animal['name']}<br/>\n"
+    if "characteristics" in animal and "diet" in animal["characteristics"]:
+        output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
+    if "locations" in animal and animal["locations"]:
+        output += f"Location: {animal['locations'][0]}<br/>\n"
+    if "characteristics" in animal and "type" in animal["characteristics"]:
+        output += f"Type: {animal['characteristics']['type']}<br/>\n"
+
+    output += "</li>\n"
+    print()
